54bb9178409fd6fc3c8891c1d928a2de3d32826f
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-09-18 09:05:51 +0300

    Update task.js - popup - added actions for exits

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,20 +1,18 @@
 const first = document.getElementById('modal_main');
-
 const second = document.getElementById('modal_success');
-
-
 first.addEventListener('click', function() {
-
   first.classList.remove('modal_active');
-  
   second.classList.add('modal_active');
-
-
 });
-
-
 second.addEventListener('click', function() {
-
     second.classList.remove('modal_active');
-  
   });
+
+const clickExit = (evt) => {
+    closePopup(evt.target.closest(".modal"));
+  };
+const exit = Array.from(document.querySelectorAll(".modal__close_times")).forEach(
+    (element) => {
+      element.addEventListener("click", clickExit);
+    }
+  );
