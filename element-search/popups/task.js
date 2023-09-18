97cd0693346a12c5f5d60ca3a27092e9b4831d4d
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-09-18 16:57:18 +0300

    Update task.js - popup - align with the task

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,22 @@
-const first = document.getElementById('modal_main');
-const second = document.getElementById('modal_success');
-first.addEventListener('click', function() {
-  first.classList.remove('modal_active');
-  second.classList.add('modal_active');
-});
-second.addEventListener('click', function() {
-    second.classList.remove('modal_active');
+const firstPopup = document.getElementById('modal_main');
+const secondPopup = document.getElementById('modal_success');
+const exit = Array.from(document.querySelectorAll('.modal__close'));
+const successBtn = document.querySelector('.btn_success');
+
+window.addEventListener('load', () => {
+    firstPopup.classList.add('modal_active');
   });
 
-const clickExit = (evt) => {
-    closePopup(evt.target.closest(".modal"));
-  };
-const exit = Array.from(document.querySelectorAll(".modal__close_times")).forEach(
-    (element) => {
-      element.addEventListener("click", clickExit);
-    }
-  );
+function ToogleModal(event) {
+    if (event.target.matches('.show-success')) {
+        firstPopup.classList.remove('modal_active');
+        secondPopup.classList.add('modal_active');}
+    else if (event.target.matches('.modal__close_times')){
+        event.target.closest('.modal').classList.remove('modal_active');}
+    };
+function exitSuccess(event) {
+    event.target.closest('.modal').classList.remove('modal_active');
+};        
+
+exit.forEach(btn => btn.addEventListener('click', ToogleModal));
+successBtn.addEventListener('click', exitSuccess)
