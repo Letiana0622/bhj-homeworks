b77ed6604b1ac17ded91d4eb46fa05298d567933
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-10-25 21:40:16 +0300

    Update task.js _ keySoloV1

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,9 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+    document.addEventListener('keyup', (e) => {
+          e.key === this.currentSymbol.innerHTML ? this.success() : this.fail();
+          });
   }
 
   success() {
