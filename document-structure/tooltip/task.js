65f1f59f74102c132040f277194c0b6f0840fd7f
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-11-25 14:29:14 +0300

    Update task.js_Tooltip_Version2(optimised)

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,23 @@
 const toolTips = Array.from(document.querySelectorAll('.has-tooltip'));
 let tips = Array.from(document.querySelectorAll('.tooltip'));
 
-toolTips.forEach((tool) => {
-    let tip = document.createElement('div');
-    let tipText = tool.getAttribute('title');
-    tip.innerText = tipText;
-    tool.insertAdjacentElement('afterend', tip);
-    tip.classList.add('tooltip');
-});
+function createTip (event) {
+        event.preventDefault(event.target.nextElementSibling);
+        console.log(event.target.nextElementSibling)
+        if (event.target.nextElementSibling === null)
+            {let tip = document.createElement('div');
+            let tipText = event.target.getAttribute('title');
+            tip.innerText = tipText;
+            event.target.insertAdjacentElement('afterend', tip);
+            tip.classList.add('tooltip');
+            let eventLeft = event.target.getBoundingClientRect().left;
+            let eventTop = event.target.getBoundingClientRect().top;
+            event.target.nextElementSibling.style.left = (parseInt(eventLeft)+10)+'px';
+            event.target.nextElementSibling.style.top = (parseInt(eventTop)+25)+'px';
+            event.target.nextElementSibling.classList.toggle('tooltip_active');}
 
-function activateTip(event) {
-   event.preventDefault();
-   let eventLeft = event.target.getBoundingClientRect().left;
-   let eventTop = event.target.getBoundingClientRect().top;
-
-   event.target.nextElementSibling.style.left = (parseInt(eventLeft)+10)+'px';
-   event.target.nextElementSibling.style.top = (parseInt(eventTop)+25)+'px';
-   event.target.nextElementSibling.classList.toggle('tooltip_active');
+        else 
+            {event.target.nextElementSibling.classList.toggle('tooltip_active')};
 };
 
 toolTips.forEach((toolTip) => {toolTip.addEventListener('click', activateTip)});
