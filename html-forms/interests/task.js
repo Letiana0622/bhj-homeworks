6656375ebd984388d62a9b7aae6bb0f5abf7ed2d
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-11-27 14:48:42 +0300

    Update task.js_html-forms/interests /version2
    added condition for code run only in browser

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,16 +1,18 @@
+if (typeof document !== 'undefined') {
+    
+let interests = Array.from(document.querySelectorAll('.interest__check'));
+
 function interestChecked (event) {
-
-let interestsList = event.target.closest('.interest');
-let related = Array.from(interestsList.querySelectorAll('.interest__check'));
-
-if (!event.target.checked === true) {
-   
+  let interestsList = event.target.closest('.interest');
+  let related = Array.from(interestsList.querySelectorAll('.interest__check'));
+  if (!event.target.checked === true) {  
     related.forEach((item) => {item.checked = false});
-}
-else {
+  }
+  else {
     related.forEach((item) => {item.checked = true});
-}
-
+  }
 };
 
 interests.forEach((interest) => {interest.addEventListener('click', interestChecked)});
+
+};
