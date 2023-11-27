54f61c429866745bbcde33ad2fda6661586942d4
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-11-27 14:41:43 +0300

    Update task.js_html-forms/chat /version2
    added input.value, input cleaning, trim, condition for empty input,   condition for code run only in browser

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,40 +1,44 @@
-const chatWidget = document.querySelector('.chat-widget');
-let chatMessages = document.querySelector('.chat-widget__messages');
+if (typeof document !== 'undefined') {
 
-chatWidget.addEventListener('click', () => {
-chatWidget.classList.add('chat-widget_active')
-})
+    const chatWidget = document.querySelector('.chat-widget');
+    let chatMessages = document.querySelector('.chat-widget__messages');
+    let chatInput = document.querySelector('.chat-widget__input');
 
+    chatWidget.addEventListener('click', () => {
+    chatWidget.classList.add('chat-widget_active')
+    }
+    );
 
-document.addEventListener('keyup', (event) => {
+    document.addEventListener('keyup', (event) => {
     if (event.code === 'Enter') {
         let data = new Date();
         let hour = data.getHours();
         let minute = data.getMinutes();
-
         let list = [
-            'Я помню чудное мгновенье',
-            'Передо мной явилась ты',
-            'Как мимолетное виденье',
-            'Как гений чистой красоты',
-            'В томленьях грусти безнадежной',
-            'В тревогах шумной суеты',
-            'Звучал мне долго голос нежный',
-            'И снились милые черты',
+                'Я помню чудное мгновенье',
+                'Передо мной явилась ты',
+                'Как мимолетное виденье',
+                'Как гений чистой красоты',
+                'В томленьях грусти безнадежной',
+                'В тревогах шумной суеты',
+                'Звучал мне долго голос нежный',
+                'И снились милые черты',
         ];
-
         let randomAnswer = list[Math.floor(Math.random() * list.length)];
-
+        let input = chatInput.value.trim();
+        if (input) {
         chatMessages.innerHTML += 
-        `<div class="message message_client"> 
-        <div class="message__time">${hour}:${minute}</div> 
-        <div class="message__text">Добрый день!</div> 
-        </div>
-        <div class="message message"> 
-        <div class="message__time">${hour}:${minute}</div> 
-        <div class="message__text">${randomAnswer}</div> 
-        </div>`
-
-      
-    }
-    })
+            `<div class="message message_client"> 
+            <div class="message__time">${hour}:${minute}</div> 
+            <div class="message__text">${input}</div> 
+            </div>
+            <div class="message message"> 
+            <div class="message__time">${hour}:${minute}</div> 
+            <div class="message__text">${randomAnswer}</div> 
+            </div>`
+        document.getElementById('chat-widget__input').value = '';
+        };
+    };
+    });
+    
+};
