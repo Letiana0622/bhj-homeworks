6378e3e00534d9ca19ffa6b1a168048941c4dc0e
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-11-29 09:13:15 +0300

    Update task.js_document-structure/cart_Version2

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,52 +2,47 @@ let controls = Array.from(document.querySelectorAll('.product__quantity-control'
 let buttons = Array.from(document.querySelectorAll('.product__add'));
 let cart = document.querySelector('.cart__products');
 
-function qtyControl (event) {
-   let qty = event.target.parentElement.querySelector('.product__quantity-value');
-   let qtyValue = parseInt(qty.innerText);
-   if (event.target.classList.contains('product__quantity-control_inc')) {qtyValue += 1}
-   else if (qtyValue === 1) {qtyValue === 1}
-   else {qtyValue -= 1};
-   qty = event.target.parentElement.querySelector('.product__quantity-value').innerText = qtyValue};
+function qtyControl(event) {
+  let qty = event.target.parentElement.querySelector('.product__quantity-value');
+  let qtyValue = parseInt(qty.innerText);
+  if (event.target.classList.contains('product__quantity-control_inc')) {qtyValue += 1;}
+  else if (qtyValue === 1) {qtyValue === 1;}
+  else {qtyValue -= 1;}
+  qty.innerText = qtyValue;
+};
 
 function addToCart (event) {
-    let checkProductId = event.target.closest('.product').getAttribute('data-id');
-    let array = Array.from(cart.querySelectorAll('.cart__product'));
+  let checkProductId = event.target.closest('.product').getAttribute('data-id');
+  let array = Array.from(cart.querySelectorAll('.cart__product'));
+  const isProductInCart = array.find(function checkCart(element, index, array) {
+    if (parseInt(element.getAttribute('data-id')) === parseInt(checkProductId)) {return true;}
+    else {return false};
+  });
 
-    function checkCart(element, index, array) {
-        let checkProductId = event.target.closest('.product').getAttribute('data-id');
-        if (parseInt(element.getAttribute('data-id')) === parseInt(checkProductId)) {return true}
-        else {return false};
-      };    
+  if (!isProductInCart) {
+    let productIdClone = event.target.closest('.product').cloneNode();
+    productIdClone.classList.remove('product');
+    productIdClone.classList.add('cart__product');
+    cart.appendChild(productIdClone);
 
-    if (array.find(checkCart) === undefined) {
-                let productId = event.target.closest('.product');
-                let productIdClone = productId.cloneNode();
-                productIdClone.classList.remove('product');
-                productIdClone.classList.add('cart__product');
-                cart.appendChild(productIdClone);
-        
-                let photo = document.createElement('img');
-                photo.classList.add('cart__product-image');
-                let srcLink = productId.querySelector('.product__image').getAttribute('src');
-                photo.setAttribute('src', srcLink);
-                productIdClone.appendChild(photo);
-        
-                let qtyCart = document.createElement('div');
-                qtyCart.classList.add('.cart__product-count');
-                let qtyProduct = productId.querySelector('.product__quantity-value').innerText;
-                qtyCart.innerText = qtyProduct
-                productIdClone.appendChild(qtyCart);
-            }
-    
-            else {Array.from(cart.querySelectorAll('.cart__product')).forEach((cartId) => {
-                if(parseInt(cartId.getAttribute('data-id')) === parseInt(checkProductId)) 
-                {
-                let qtyToInc = cartId.getElementsByTagName('div')[0].textContent;
-                let qtyToTake = event.target.previousElementSibling.children[1].textContent;
-                cartId.getElementsByTagName('div')[0].textContent = parseInt(qtyToInc) + parseInt(qtyToTake);
-                    } })};
-        };            
+    let photo = document.createElement('img');
+    photo.classList.add('cart__product-image');
+    photo.setAttribute('src', event.target.closest('.product').querySelector('.product__image').getAttribute('src'));
+    productIdClone.appendChild(photo);
+
+    let qtyCart = document.createElement('div');
+    qtyCart.classList.add('.cart__product-count');
+    qtyCart.innerText = event.target.closest('.product').querySelector('.product__quantity-value').innerText;
+    productIdClone.appendChild(qtyCart);
+    }
+  else if(isProductInCart) {
+    Array.from(cart.querySelectorAll('.cart__product')).forEach((cartId) => {
+      let qtyToInc = cartId.getElementsByTagName('div')[0].textContent;
+      let qtyToTake = event.target.previousElementSibling.children[1].textContent;
+      cartId.getElementsByTagName('div')[0].textContent = parseInt(qtyToInc) + parseInt(qtyToTake);
+    });
+  };
+};            
 
 controls.forEach((control) => {control.addEventListener('click', qtyControl)})
 buttons.forEach((button) => {button.addEventListener('click', addToCart)});
