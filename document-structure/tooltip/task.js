38d78aee183e115320d35c121da298af7379defc
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-11-29 09:05:46 +0300

    Update task.js_document-structure/tooltip_Version3

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,23 +1,26 @@
-const toolTips = Array.from(document.querySelectorAll('.has-tooltip'));
+let toolTips = Array.from(document.querySelectorAll('.has-tooltip'));
 let tips = Array.from(document.querySelectorAll('.tooltip'));
 
-function createTip (event) {
-        event.preventDefault(event.target.nextElementSibling);
-        console.log(event.target.nextElementSibling)
-        if (event.target.nextElementSibling === null)
-            {let tip = document.createElement('div');
-            let tipText = event.target.getAttribute('title');
-            tip.innerText = tipText;
-            event.target.insertAdjacentElement('afterend', tip);
-            tip.classList.add('tooltip');
-            let eventLeft = event.target.getBoundingClientRect().left;
-            let eventTop = event.target.getBoundingClientRect().top;
-            event.target.nextElementSibling.style.left = (parseInt(eventLeft)+10)+'px';
-            event.target.nextElementSibling.style.top = (parseInt(eventTop)+25)+'px';
-            event.target.nextElementSibling.classList.toggle('tooltip_active');}
+function createTip(event) {
+  event.preventDefault();
+  let isNextNotExists = event.target.nextElementSibling === null;
+  const isNextContainsTooltip = event.target.nextElementSibling !== null? event.target.nextElementSibling.classList.contains('tooltip'): null;
+  
+  if (isNextNotExists || !isNextContainsTooltip) {
+    let tip = document.createElement('div');
+    tip.innerText = event.target.getAttribute('title');
+    tip.classList.add('tooltip');
+    event.target.insertAdjacentElement('afterend', tip);
+    let eventLeft = event.target.getBoundingClientRect().left;
+    let eventTop = event.target.getBoundingClientRect().top;
+    event.target.nextElementSibling.style.left = (parseInt(eventLeft)+10)+'px';
+    event.target.nextElementSibling.style.top = (parseInt(eventTop)+25)+'px';
+  };
 
-        else 
-            {event.target.nextElementSibling.classList.toggle('tooltip_active')};
+    event.target.nextElementSibling.classList.toggle('tooltip_active');
 };
 
-toolTips.forEach((toolTip) => {toolTip.addEventListener('click', activateTip)});
+toolTips.forEach((toolTip) => {
+  toolTip.addEventListener('click', createTip);
+  }
+);
