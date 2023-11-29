3c4e39457c9c25c25324234c8d0202d087aa7f4d
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-11-29 09:09:52 +0300

    Update task.js_document-structure/todo_Version2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,18 +1,35 @@
 let input = document.getElementById('task__input');
 let tasksList = document.querySelector('.tasks__list');
-let tasksRemove = Array.from(tasksList.querySelectorAll('.task__remove'));
+let button = document.querySelector('.tasks__add');
 
-document.addEventListener('keyup', (event) => {
-if (event.code === 'Enter') {
-    let newToDo = input.value;
-    tasksList.innerHTML +=
-    `<div class="task"> <div class="task__title"> ${newToDo} </div> <a href="#" class="task__remove">&times;</a> </div>`
+function delay(customDiv) {
+  customDiv.addEventListener('click', ()=> {
+    customDiv.remove();
     }
-});
-
-function Remove(event) {
-event.target.closest('task').remove();
+  );
 };
 
-tasksRemove.forEach((taskRemove) => {taskRemove.addEventListener('click', Remove)})
+function addTask(event) {
+    event.preventDefault();
+    let newToDo = input.value.trim();
+    if (newToDo) {
+      let customDiv = document.createElement('div');
+      customDiv.classList.add('task');
+      tasksList.appendChild(customDiv);
 
+      let titleDiv = document.createElement('div');
+      titleDiv.classList.add('task__title');
+      titleDiv.innerText = `${newToDo}`
+      customDiv.appendChild(titleDiv);
+        
+      let linkA = document.createElement('a');
+      customDiv.appendChild(linkA);
+      linkA.setAttribute('href', '#');
+      linkA.classList.add('task__remove');
+      linkA.innerHTML = `&times;`;
+
+      delay(customDiv);
+      };
+    };
+
+button.addEventListener('click', addTask);
