4209a9acfb8c13f4c084fc2c91755f5a2dceb036
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-12-05 16:08:12 +0300

    Update task.js_document-structure/tooltip_version5
    *corrected mistake with preventdefault
    * remove method replaced with toggle

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,22 @@
 let toolTips = Array.from(document.querySelectorAll('.has-tooltip'));
-let tips = Array.from(document.querySelectorAll('.tooltip'));
 
 function createTip(event) {
+  
+  event.preventDefault();
+
   if (event.target.children[0]) {
-    event.target.children[0].remove();
+    event.target.children[0].classList.toggle('tooltip_active');
   }
   else {
-    event.preventDefault();
     let eventLeft = parseInt(event.target.getBoundingClientRect().left)+10+'px';
     let eventTop = parseInt(event.target.getBoundingClientRect().top)+25+'px';
     event.target.innerHTML += `
-    <div class="tooltip" style="left: ${eventLeft}; top: ${eventTop}">
+    <div class="tooltip tooltip_active" style="left: ${eventLeft}; top: ${eventTop}">
       ${event.target.getAttribute('title')}
     </div>
     `;
-    document.getElementsByClassName('tooltip')[0].classList.add('tooltip_active'); 
-  };
-};
+  }
+}
 
 toolTips.forEach((toolTip) => {
   toolTip.addEventListener('click', createTip);
