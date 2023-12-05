8662f1da587c8a943de56de7cdb41d84c2b1932f
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-12-05 16:13:26 +0300

    Update task.js_document-structure/todo_version4
    *insertAdjacentHTML is used to create new HTML elements
    *optimized removeTask function accordingly

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,29 +2,27 @@ let input = document.getElementById('task__input');
 let tasksList = document.querySelector('.tasks__list');
 let button = document.querySelector('.tasks__add');
 
-function removeTask(customDiv) {
-  for (let i=0; i < customDiv.length; i++) {
-    customDiv[i].addEventListener('click', () => {
-      if (customDiv.length > 1) {
-        customDiv[i].parentElement.remove();
-      }
-      else {
-        document.querySelector('.task').remove();
-      };
-    });
+function removeTask(removeButton) {
+    removeButton.addEventListener('click', () => {
+      removeButton.parentElement.remove();
+    })
   };
-};
 
 function addTask(event) {
   event.preventDefault();
   let newToDo = input.value.trim();
   if (newToDo) {
-    tasksList.innerHTML +=
-     `<div class="task"> <div class="task__title"> ${newToDo} </div> <a href="#" class="task__remove">&times;</a> </div>`
+    tasksList.insertAdjacentHTML( 'beforeend',
+     `<div class="task"> 
+     <div class="task__title"> ${newToDo} </div> 
+     <a href="#" class="task__remove">&times;</a> 
+     </div>`
+    )
   };
+  let removeButtons = Array.from(document.querySelectorAll('.task__remove'))
+  let removeButton = removeButtons[removeButtons.length - 1];
   input.value = '';
-  let customDiv = document.getElementsByClassName('task__remove');
-  removeTask(customDiv);
+  removeTask(removeButton);
 };
 
 button.addEventListener('click', addTask);
