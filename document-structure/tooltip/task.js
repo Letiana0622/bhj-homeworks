036f02473fd0bc0105a83a68c51a770b346b5ac5
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-12-07 13:59:45 +0300

    Update task.js_document-structure/tooltip_version6
    *children HTML structure for new elements is corrected to siblings approach
    * the text in the link and in the tip is compared to check click on the tip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,16 +1,17 @@
 let toolTips = Array.from(document.querySelectorAll('.has-tooltip'));
 
 function createTip(event) {
-  
   event.preventDefault();
+  let isSiblingExists = event.target.nextElementSibling !== null;
+  const isTipClicked = isSiblingExists? event.target.getAttribute('title') === event.target.nextElementSibling.textContent: false;
 
-  if (event.target.children[0]) {
-    event.target.children[0].classList.toggle('tooltip_active');
+  if (isTipClicked) {
+    event.target.nextElementSibling.classList.toggle('tooltip_active');
   }
   else {
     let eventLeft = parseInt(event.target.getBoundingClientRect().left)+10+'px';
     let eventTop = parseInt(event.target.getBoundingClientRect().top)+25+'px';
-    event.target.innerHTML += `
+    event.target.insertAdjacentHTML('afterend',`
     <div class="tooltip tooltip_active" style="left: ${eventLeft}; top: ${eventTop}">
       ${event.target.getAttribute('title')}
     </div>
