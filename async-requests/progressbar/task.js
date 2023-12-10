13b7c189e4e2face1ae005af167d5b60be3ba274
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-12-10 13:17:00 +0300

    Update task.js_async-requests/progressbar_version2
    *xhr.readystatechange is replaced with events of xhr.upload

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,32 @@
 if (typeof document !== 'undefined') {
   const formElement = document.getElementById('form');
   const progressBar = document.getElementById('progress');
+  const fileChosen = document.getElementById('file');
+  const fileDescr = document.querySelector('.input__wrapper-desc');
 
   formElement.addEventListener('submit', (e) => {
     e.preventDefault();
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', () => {
-      if (xhr.readyState === xhr.OPENED) {progressBar.setAttribute('value','0.25')};
-      if (xhr.readyState === xhr.HEADERS_RECEIVED) {progressBar.setAttribute('value','0.5')};
-      if (xhr.readyState === xhr.LOADING) {progressBar.setAttribute('value','0.75')};
-      if (xhr.readyState === xhr.DONE) {
-        console.log(xhr.responseText);
-        progressBar.setAttribute('value','1');
-        setTimeout(() => {progressBar.setAttribute('value','0.0')}, 3000);
-      };
-    });
+
+    xhr.upload.onloadstart = function() {
+      progressBar.setAttribute('value','0.1')
+    };
+    
+    xhr.upload.onprogress = function(event) {
+      progressBar.setAttribute('value',event.loaded);
+    };
+    
+    xhr.upload.onload = function() {
+      console.log(xhr.responseText);
+      progressBar.setAttribute('value','1');
+      setTimeout(() => {
+        progressBar.setAttribute('value','0.0');
+        fileChosen.value = '';
+        fileDescr.textContent = '';
+      }
+      , 3000);
+    };
+    
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     const formData = new FormData(formElement);
