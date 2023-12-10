9ff7f2594c1ac04734893162eea4a2998ae6a4f3
Letiana0622 <107596213+Letiana0622@users.noreply.github.com>
2023-12-10 14:10:44 +0300

    Update task.js_async-requests/progressbar_Version3
    *used events of xhr.upload
    * added console status tracking

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -9,16 +9,18 @@ if (typeof document !== 'undefined') {
     const xhr = new XMLHttpRequest();
 
     xhr.upload.onloadstart = function() {
-      progressBar.setAttribute('value','0.1')
+      progressBar.setAttribute('value','0.3');
+      console.log("Начало загрузки данных");
     };
     
     xhr.upload.onprogress = function(event) {
-      progressBar.setAttribute('value',event.loaded);
+      progressBar.setAttribute('value','0.7');
+      console.log(`Отправлено ${event.loaded} из ${event.total} байт`);
     };
     
     xhr.upload.onload = function() {
-      console.log(xhr.responseText);
       progressBar.setAttribute('value','1');
+      console.log("Данные успешно отправлены");
       setTimeout(() => {
         progressBar.setAttribute('value','0.0');
         fileChosen.value = '';
@@ -26,10 +28,15 @@ if (typeof document !== 'undefined') {
       }
       , 3000);
     };
+
+    xhr.upload.onerror = function() {
+      console.log(`Произошла ошибка во время отправки: ${xhr.status}`);
+    };
     
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     const formData = new FormData(formElement);
     xhr.send(formData);
+    xhr.onload = () => console.log(xhr.response);
   });
 };
